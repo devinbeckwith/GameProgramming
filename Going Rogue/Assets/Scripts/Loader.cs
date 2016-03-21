840ebf85ea4ccc252ec149a93b8f0d73478f7afe
nkatica <nkatica@bennington.edu>
2016-03-21 11:41:45 -0400

    Moving Script Along

---
+++ Going Rogue/Assets/Scripts/Loader.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+	public GameObject gameManager;
+		
+	void Awake (){
+			if (GameManager.instance == null)
+				Instantiate(gameManager);
+		}
+}
